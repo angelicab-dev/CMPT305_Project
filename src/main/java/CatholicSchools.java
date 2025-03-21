d644a5ac728d5cc5db812abef8b3afbf1ed44443
kamilm3 <116315118+kamilm3@users.noreply.github.com>
2025-03-20 19:46:09 -0600

    Removed unused BufferedReader imports from CatholicSchools and PublicSchools classes

--- src/main/java/CatholicSchools.java
+++ src/main/java/CatholicSchools.java
@@ -1,8 +1,6 @@
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvValidationException;
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
