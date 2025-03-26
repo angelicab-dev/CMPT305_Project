906604451a06e4960d9970db7bfb814050349bb1
angelicab20 <anbilliones@gmail.com>
2025-03-25 21:38:40 -0600

    Added test class for Attractions

---
+++ src/test/java/AttractionsTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AttractionsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void readData() {
+    }
+
+    @Test
+    void getTotalAttractions() {
+    }
+
+    @Test
+    void findByFacilityName() {
+    }
+
+    @Test
+    void filterByAttractionType() {
+    }
+
+    @Test
+    void getAttractions() {
+    }
+}
