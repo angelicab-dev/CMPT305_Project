5496856cc26b161f828e99cc399aa9e8fb679477
kamilm3 <116315118+kamilm3@users.noreply.github.com>
2025-03-30 20:06:59 -0600

    commented School.java

--- src/main/java/School.java
+++ src/main/java/School.java
@@ -1,3 +1,6 @@
+/**
+ * This class represents a school with its details
+ */
 public class School {
     private String schoolName;
     private String street;
@@ -6,6 +9,17 @@ public class School {
     private String gradeLevel;
     private Location location;
 
+    /**
+     * Constructor to initialize a school object
+     *
+     * @param schoolName  Name of the school
+     * @param street      Street address of the school
+     * @param postalCode  Postal code of the school
+     * @param phoneNumber Phone number of the school
+     * @param gradeLevel  Grade level of the school
+     * @param latitude    Latitude of the school's location
+     * @param longitude   Longitude of the school's location
+     */
     public School(String schoolName, String street, String postalCode,
                   String phoneNumber, String gradeLevel, String latitude, String longitude) {
         this.schoolName = schoolName;
@@ -16,26 +30,50 @@ public class School {
         this.location = new Location(latitude, longitude);
     }
 
+    /**
+     * Returns the name of the school.
+     * @return the school name.
+     */
     public String getSchoolName() {
         return schoolName;
     }
 
+    /**
+     * Returns the street address of the school.
+     * @return the street address.
+     */
     public String getStreet() {
         return street;
     }
 
+    /**
+     * Returns the postal code of the school.
+     * @return the postal code.
+     */
     public String getPostalCode() {
         return postalCode;
     }
 
+    /**
+     * Returns the phone number of the school.
+     * @return the phone number.
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Returns the location of the school.
+     * @return the location object containing latitude and longitude.
+     */
     public Location getLocation() {
         return location;
     }
 
+    /**
+     * Returns the grade level of the school.
+     * @return the grade level.
+     */
     public String getGradeLevel() {
         return gradeLevel;
     }
