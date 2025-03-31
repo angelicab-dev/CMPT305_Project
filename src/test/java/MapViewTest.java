1ff343ca6764813eac362d490d8c3393b500710f
JheaneyPerico <jheaneyperico@gmail.com>
2025-03-30 20:38:26 -0600

    Added Empty MapViewTest

---
+++ src/test/java/MapViewTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapViewTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addMarker() {
+    }
+
+    @Test
+    void clearMarkers() {
+    }
+}
