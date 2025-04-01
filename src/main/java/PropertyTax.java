ee4031c7d8cd6cf4a32387eb6e17be80cbb5eae9
angelicab20 <anbilliones@gmail.com>
2025-03-31 22:54:52 -0600

    Created Propert Tax class; it has a constructor and calculate method

---
+++ src/main/java/PropertyTax.java
@@ -0,0 +1,24 @@
+public class PropertyTax {
+
+
+    /*
+    To calculate property tax, the following are defined in Tax Breakdown:
+    1. City budget
+    2. Total assessed value of all properties in our city (extract from Property Assessments class)
+    3. budget / total assessed value of all properties = municipal tax rate
+
+    Property tax calculation:
+    your property assessed value * municipal tax rate = your municipal property taxes
+    - municipal tax rate (2024) = 8.9%
+     */
+
+    private int propertyValue;
+    public PropertyTax(int assessedValue) {
+        this.propertyValue = assessedValue;
+    }
+
+    public double calculatePropertyTax() {
+        double taxRate = 0.089;
+        return propertyValue * taxRate;
+    }
+}
